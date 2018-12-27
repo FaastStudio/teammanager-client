9cb775d874f9e49069c05557d43db4de76ed3a98
kilianstallz <mobil.kilian@live.com>
2018-12-27 23:55:29 +0100

    History Mode Middelware

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
 const app = express()
+app.use(history())
 app.use(serveStatic(path.join(__dirname, 'dist')))
+app.get('*', function(req, res) {
+  // eslint-disable-next-line
+  res.sendFile(__dirname + '/dist/index.html')
+})
 const port = process.env.PORT || 80
 app.listen(port)
