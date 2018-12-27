b3f9d3aaacdb0c4dff12667045fd082b61b93a8b
kilianstallz <mobil.kilian@live.com>
2018-12-28 00:15:23 +0100

    Routes Fix

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
+var history = require('connect-history-api-fallback')
 const app = express()
 app.use(history())
 app.use(serveStatic(path.join(__dirname, 'dist')))
